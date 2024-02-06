81e40aa1e29cf2ad80e2229da21ebba47c885ea4
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-06 21:23:49 +0600

    обновил characterInformationOutput и тест

--- src/js/_tests_/destructuring.test.js
+++ src/js/_tests_/destructuring.test.js
@@ -1,3 +1,47 @@
 // прописать import все файлы JS
-import characterInformationOutput from '../destructuring';
+import characterInformationOutput from '../Destructuring';
 // начать тесты
+
+test('проверка на наличие атак в объекте', () => {
+  const character = {
+    name: 'Лучник',
+    fdfdf: [],
+  };
+  expect(() => {
+    characterInformationOutput(character);
+  }).toThrow();
+});
+
+test('Обработка атак', () => {
+  const character = {
+    name: 'Лучник',
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+  const resFunc = characterInformationOutput(character);
+  expect(resFunc).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
+});
--- src/js/destructuring.js
+++ src/js/destructuring.js
@@ -1,4 +1,7 @@
 function characterInformationOutput(characterObject) {
+  if (!characterObject.special) {
+    throw new Error('Отсутствуют атаки');
+  }
   const result = [];
   for (let i = 0; i < characterObject.special.length; i += 1) {
     result.push({});
@@ -11,38 +14,7 @@ function characterInformationOutput(characterObject) {
   }
   return result;
 }
-/* const character = {
-  name: 'Лучник',
-  type: 'Bowman',
-  health: 50,
-  level: 3,
-  attack: 40,
-  defence: 10,
-  special: [
-    {
-      id: 8,
-      name: 'Двойной выстрел',
-      icon: 'http://...',
-      description: 'Двойной выстрел наносит двойной урон',
-    },
-    {
-      id: 9,
-      name: 'Нокаутирующий удар',
-      icon: 'http://...',
-    },
-    {
-      id: 34344,
-      name: 'Нокаутирующий ',
-      icon: 'http://...',
-    },
-    {
-      id: 8909890,
-      name: ' удар',
-      icon: 'http://...',
-      description: 'двойной урон',
-    },
-  ],
-}; */
+
 //  characterInformationOutput(character);
 
 export default characterInformationOutput;
